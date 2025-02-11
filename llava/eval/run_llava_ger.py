8be92c25a8fec22aa0a88eb0378dd217cfa5ee47
WilsonMqz <1746387101@qq.com>
2025-02-11 16:28:56 +0800

    Add files via upload

---
+++ llava/eval/run_llava_ger.py
@@ -0,0 +1,179 @@
+import argparse
+import torch
+
+from llava.constants import (
+    IMAGE_TOKEN_INDEX,
+    DEFAULT_IMAGE_TOKEN,
+    DEFAULT_IM_START_TOKEN,
+    DEFAULT_IM_END_TOKEN,
+    IMAGE_PLACEHOLDER,
+)
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.model.builder import load_pretrained_model
+from llava.utils import disable_torch_init
+from llava.mm_utils import (
+    process_images,
+    tokenizer_image_token,
+    get_model_name_from_path,
+)
+
+from PIL import Image
+
+import requests
+from PIL import Image
+from io import BytesIO
+import re
+import os
+import sys
+
+import numpy as np
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+
+def image_parser(args):
+    out = args.image_file.split(args.sep)
+    return out
+
+
+def load_image(image_file):
+    if image_file.startswith("http") or image_file.startswith("https"):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert("RGB")
+    else:
+        image = Image.open(image_file).convert("RGB")
+    return image
+
+
+def load_images(image_files):
+    out = []
+    for image_file in image_files:
+        image = load_image(image_file)
+        out.append(image)
+    return out
+
+
+def eval_model(args):
+    # Model
+    disable_torch_init()
+
+    model_name = get_model_name_from_path(args.model_path)
+    tokenizer, model, image_processor, context_len = load_pretrained_model(
+        args.model_path, args.model_base, model_name
+    )
+
+    qs = args.query
+    image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+    if IMAGE_PLACEHOLDER in qs:
+        if model.config.mm_use_im_start_end:
+            qs = re.sub(IMAGE_PLACEHOLDER, image_token_se, qs)
+        else:
+            qs = re.sub(IMAGE_PLACEHOLDER, DEFAULT_IMAGE_TOKEN, qs)
+    else:
+        if model.config.mm_use_im_start_end:
+            qs = image_token_se + "\n" + qs
+        else:
+            qs = DEFAULT_IMAGE_TOKEN + "\n" + qs
+
+    if "llama-2" in model_name.lower():
+        conv_mode = "llava_llama_2"
+    elif "mistral" in model_name.lower():
+        conv_mode = "mistral_instruct"
+    elif "v1.6-34b" in model_name.lower():
+        conv_mode = "chatml_direct"
+    elif "v1" in model_name.lower():
+        conv_mode = "llava_v1"
+    elif "mpt" in model_name.lower():
+        conv_mode = "mpt"
+    else:
+        conv_mode = "llava_v0"
+
+    if args.conv_mode is not None and conv_mode != args.conv_mode:
+        print(
+            "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
+                conv_mode, args.conv_mode, args.conv_mode
+            )
+        )
+    else:
+        args.conv_mode = conv_mode
+
+    conv = conv_templates[args.conv_mode].copy()
+    conv.append_message(conv.roles[0], qs)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
+
+    # image_files = image_parser(args)
+    image_files_s = args.image_file
+
+    for i in range(len(image_files_s)):
+        image_files = [image_files_s[i]]
+        print(image_files)
+        labels_onehot = args.labels[i]
+        labels = np.take(args.taglist, np.where(labels_onehot == 1)[0])
+        images = load_images(image_files)
+        image_sizes = [x.size for x in images]
+
+        images_tensor = process_images(
+            images,
+            image_processor,
+            model.config
+        ).to(model.device, dtype=torch.float16)
+
+        input_ids = (
+            tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt")
+            .unsqueeze(0)
+            .cuda()
+        )
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=images_tensor,
+                image_sizes=image_sizes,
+                do_sample=True if args.temperature > 0 else False,
+                temperature=args.temperature,
+                top_p=args.top_p,
+                num_beams=args.num_beams,
+                max_new_tokens=args.max_new_tokens,
+                use_cache=True,
+            )
+
+        outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+        print("Predicts:{}, True labels:{}".format(outputs, labels))
+
+
+if __name__ == "__main__":
+    model_path = "D:\code\python\weakly_supervised\othercode\LLaVA\ckpt/llava-7b"
+
+    # prompt = "Fill in the blank: this photo contains {}."
+    # prompt = "Output the Top-5 names of objects with most distinctive features in this photo. Answer format: <comma separated list of objects>."
+    prompt = "Write a short text prompt that describes this picture and the existing objects."
+    tag_file = os.path.join(f'D:\datasets\mll_datasets/voc', 'voc_ram_taglist.txt')
+    with open(tag_file, "r", encoding="utf-8") as f:
+        taglist = [line.strip() for line in f]
+    X = np.load(os.path.join(f'D:\datasets\mll_datasets/voc', 'formatted_val_images.npy'))
+    Y = np.load(os.path.join(f'D:\datasets\mll_datasets/voc', 'formatted_val_labels.npy'))
+    X = X[:10]
+    Y = Y[:10]
+
+    image_file = ["D:\datasets\mll_datasets/voc/VOCdevkit/VOC2012/JPEGImages/" + line for line in X]
+
+    args = type('Args', (), {
+        "model_path": model_path,
+        "model_base": None,
+        "model_name": get_model_name_from_path(model_path),
+        "query": prompt,
+        "conv_mode": None,
+        "image_file": image_file,
+        "labels": Y,
+        "taglist": taglist,
+        "sep": ",",
+        "temperature": 0,
+        "top_p": None,
+        "num_beams": 1,
+        "max_new_tokens": 512
+    })()
+
+    eval_model(args)
